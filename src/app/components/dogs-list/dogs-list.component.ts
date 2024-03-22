5cd6985f3e2aca83fbcb5a28b893f924c4efffb9
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2024-03-22 12:57:20 +0100

    create new interface for model Dog

--- src/app/components/dogs-list/dogs-list.component.ts
+++ src/app/components/dogs-list/dogs-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {DogsService} from "../../service/dogs/dogs.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-dogs-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dogs-list.component.html',
   styleUrl: './dogs-list.component.scss'
 })
-export class DogsListComponent {
 
+export class DogsListComponent {
+  constructor(readonly  dogsService: DogsService) { }
 }
